50936c9ea90764a2d998344d6eaf44d3d562527f
Chris McComb <ccmcc2012@gmail.com>
2020-08-24 22:54:12 -0400

    SO MANY TESTS

---
+++ tests/ukrainian.rs
@@ -0,0 +1,35 @@
+#[cfg(test)]
+mod ukrainian_tests {
+    use stop_words;
+
+    const TARGET_FULL: &str = "ukrainian";
+    const TARGET_ISO_632_1: &str = "uk";
+    const TARGET_ISO_632_2T: &str = "ukr";
+
+    #[test]
+    fn compare_full_to_2letter() {
+        let x = stop_words::get(TARGET_FULL);
+        let y = stop_words::get(TARGET_ISO_632_1);
+        for idx in 0..x.len() {
+            assert_eq!(x[idx], y[idx])
+        }
+    }
+
+    #[test]
+    fn compare_full_to_3letter() {
+        let x = stop_words::get(TARGET_FULL);
+        let y = stop_words::get(TARGET_ISO_632_2T);
+        for idx in 0..x.len() {
+            assert_eq!(x[idx], y[idx])
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn check_nltk() {
+        let x = stop_words::get_nltk(TARGET_FULL);
+        for idx in 0..x.len() {
+            println!("{}", x[idx])
+        }
+    }
+}
