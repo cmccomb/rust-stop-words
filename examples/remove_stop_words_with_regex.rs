f764b6e3b772d13e85eb1fd77f99ada7ca53853f
Chris McComb <ccmcc2012@gmail.com>
2021-12-27 15:01:09 -0500

    Updated example

--- examples/remove_stop_words_with_regex.rs
+++ examples/remove_stop_words_with_regex.rs
@@ -1,23 +1,26 @@
 use stop_words::{get, LANGUAGE};
-use human_regex::{exactly, or, punctuation, whitespace, word_boundary, one_or_more};
+use human_regex::{exactly, or, punctuation, whitespace, word_boundary, one_or_more, text};
 
 fn main() {
     // Read in a file
-    let text = std::fs::read_to_string("foreword.txt").expect("Cannot read file");
+    let document = std::fs::read_to_string("foreword.txt").expect("Cannot read file");
 
     // Print the contents
-    println!("Original text:\n{}", text);
+    println!("Original text:\n{}", document);
 
     // Get the stopwords
-    let words = get(LANGUAGE::English);
+    let mut new_words = Vec::new();
+    for word in get(LANGUAGE::English) {
+        new_words.push(text(word));
+    }
 
     // Remove punctuation and lowercase the text to make parsing easier
-    let text = text.to_ascii_lowercase();
+    let lowercase_doc = document.to_ascii_lowercase();
     let regex_for_punctuation = one_or_more(punctuation());
-    let text_without_punctuation = regex_for_punctuation.to_regex().replace_all(&*text, "");
+    let text_without_punctuation = regex_for_punctuation.to_regex().replace_all(&*lowercase_doc, "");
 
     // Make a regex to match stopwords with trailing spaces and punctuation
-    let regex_for_stop_words = word_boundary() + exactly(1, or(&words)) + word_boundary() + one_or_more(whitespace());
+    let regex_for_stop_words = word_boundary() + exactly(1, or(&new_words)) + word_boundary() + one_or_more(whitespace());
 
     // Remove stop words
     let clean_text = regex_for_stop_words.to_regex().replace_all(&*text_without_punctuation, "");
