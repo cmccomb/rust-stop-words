be38c47ab5cb17d138f41993e6dd224306508cf3
Chris McComb <ccmcc2012@gmail.com>
2023-04-01 15:39:00 -0400

    Simplify simplify

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,16 @@ mod language_names;
 pub use language_names::LANGUAGE;
 use serde_json::Value;
 
-/// This function  fetches stop words for a language using either a member of the `LANGUAGE` enum,
+/// This function fetches stop words for a language using either a member of the `LANGUAGE` enum,
 /// or a two-character ISO language name as either a `str` or a `String` type.
 /// ```
 /// let first_list = stop_words::get("ar");
 /// let second_list = stop_words::get(stop_words::LANGUAGE::Arabic);
 /// assert_eq!(first_list, second_list)
 /// ```
-pub fn get<T>(input_language: T) -> Vec<String>
-where
-    T: Into<String>,
-{
+pub fn get<T: Into<String>>(input_language: T) -> Vec<String> {
     // Check the input
-    let language_name_as_enum = LANGUAGE::from(input_language.into());
+    let language_name_as_string = input_language.into();
 
     // Get the bytes
     let json_as_bytes: &[u8] = if cfg!(feature = "nltk") {
@@ -29,13 +26,13 @@ where
     };
 
     // Get the JSON
-    let mut json: Value = serde_json::from_slice(json_as_bytes)
+    let json: Value = serde_json::from_slice(json_as_bytes)
         .expect("Could not read JSON file from Stopwords ISO.");
 
     // Get the words
-    json[String::from(language_name_as_enum)]
+    json.get(language_name_as_string.clone()).unwrap_or_else(|| panic!("The '{language_name_as_string}' language is not recognized. Please check the documentation for a supported list of languages.")).clone()
         .as_array_mut()
-        .expect("The '{input_language}' language is not recognized. Please check the documentation for a supported list of languages.")
+        .expect("The referenced value is not a mutable array.")
         .iter()
         .map(Value::to_string)
         .collect()
