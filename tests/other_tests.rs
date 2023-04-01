6f8caa543c2397f5f60d0c5fc03187fd3e927b94
Chris McComb <ccmcc2012@gmail.com>
2023-04-01 14:11:42 -0400

    For lulz

--- tests/other_tests.rs
+++ tests/other_tests.rs
@@ -4,7 +4,7 @@ mod panic_tests {
     #[test]
     #[should_panic]
     fn bad_language_code_1() {
-        let x = stop_words::get("zz");
+        let x = stop_words::get("dothraki");
         for y in x {
             println!("{}", y);
         }
