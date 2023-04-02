c178806a9b5c8db821752022f89862a0ef6725e9
Chris McComb <ccmcc2012@gmail.com>
2023-04-02 12:30:48 -0400

    Example updated

--- examples/vec_to_hash.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashSet;
-
-fn main() {
-    // Get the stop words
-    let vec = stop_words::get(stop_words::LANGUAGE::English);
-
-    // Convert to hash set
-    let _set: HashSet<String> = vec.into_iter().collect();
-}
